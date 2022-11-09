a9dd1948a82f0bb386a4ba682de1f112b40d31ef
xzr1ley <pocset2@gmail.com>
2022-11-09 23:53:18 +0300

    drop-area v2 with broken bounds

--- js/edesign/src/components/area/Area.jsx
+++ js/edesign/src/components/area/Area.jsx
@@ -3,11 +3,30 @@ import Draggable from 'react-draggable'
 import { useRef } from 'react'
 import './area.css'
 
-const Area = () =>{
+
+const isRectInArea = (rect, area) => (
+    rect.top >= area.top
+    && rect.left >= area.left
+    && rect.top + rect.height <= area.top + area.height   
+    && rect.left + rect.width <= area.left + area.width
+  )
+  
+  const getBoundsArea = (rect, area) => ({
+    left: area.left, 
+    top: area.top, 
+    right: (area.left + area.width - rect.width), 
+    bottom: (area.top + area.height - rect.height)
+  }) 
+
+
+const Area = () => {
     const [drag, setDrag] = useState(false)
 
+    const imgPlaceRef = useRef(null)
     const areaRef = useRef(null)
-
+    const [transform, setTransform] = useState('none');
+    const [boundsArea, setBoundsArea] = useState(false)
+    
     function dragStartHandler(e) {
         e.preventDefault()
         setDrag(true)
@@ -25,35 +44,61 @@ const Area = () =>{
 
         let newimg = document.createElement('img')
         newimg.classList.add('newimg')
-        areaRef.current.appendChild(newimg)
+        imgPlaceRef.current.append(newimg)
         newimg.src = window.URL.createObjectURL(files[0])
 
         setDrag(false)
+    }
+
+        const handleEvent = (e, data) => {
+            if (boundsArea) return true
+        
+            if (data && imgPlaceRef &&  imgPlaceRef.current && areaRef && areaRef.current) {
+        
+              const dragRC = imgPlaceRef.current.getBoundingClientRect()
+              const areaRC = areaRef.current.getBoundingClientRect()
+              if (isRectInArea(dragRC, areaRC)){
+                 areaRef.current.style.background = 'grey'   
+              } else {
+                 areaRef.current.style.background = 'black'   
+              }
+              if (isRectInArea(dragRC, areaRC) && e.type === 'mouseup' ){
+                areaRef.current.style.background = 'black'
+                setBoundsArea(getBoundsArea(dragRC, areaRC))
+              } 
+            }
+        
     } 
-    console.log(areaRef.current);
+    console.log(imgPlaceRef.current);
 
 
 
     return(
         <div className='Area'>
-            {drag
-                ? <div
+                <Draggable
+                    bounds={boundsArea}
+                    onDrag={handleEvent}
+                    onStart={handleEvent}
+                    onStop={handleEvent}
+                    onMouseDown={handleEvent}
+                    onMouseUp={handleEvent}
+                    onTouchStart={handleEvent}
+                    onTouchEnd={handleEvent}
+                >
+                    <div className='place-for-img' ref={imgPlaceRef}></div>
+                </Draggable>
+            
+                <div
+                    ref = {areaRef}
                     className='drop-area'
                     onDragStart = {e => dragStartHandler(e)}
                     onDragLeave = {e => dragLeaveHandler(e)}
                     onDragOver = {e => dragStartHandler(e)}
                     onDrop = {e => onDropHandler(e)}
-                    >Отпустите файл для загрузки</div>
-                : <div
-                    className='drop-area'
-                    onDragStart = {e => dragStartHandler(e)}
-                    onDragLeave = {e => dragLeaveHandler(e)}
-                    onDragOver = {e => dragStartHandler(e)}
-                >Перетащите файл для загрузки</div>  
-            }
-            <Draggable>
-            <div ref={areaRef}></div>
-            </Draggable>
+                    ></div>
+                
+            
+            
         </div>
     )
 
